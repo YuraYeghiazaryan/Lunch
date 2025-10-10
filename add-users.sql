785108520aa4d7cf99b812b2f915eefb0328f5d5
Yura Yeghiazaryan <yura.yeghiazaryan@mathschool.com>
2025-10-10 16:04:00 +0400

    add product option

---
+++ add-users.sql
@@ -0,0 +1,6 @@
+INSERT INTO sovats."user"."user" (email, password, first_name, last_name)
+VALUES ('myEmail', 'pass', 'Yura', 'Yeghiazaryan');
+
+INSERT INTO sovats."user"."user" (email, password, first_name, last_name)
+VALUES ('myEmail_1', 'pass', 'Khachik', 'Yeritsyan');
+
